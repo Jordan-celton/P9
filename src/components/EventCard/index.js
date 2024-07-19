b9f4f5534ac80db47f200934b1cea9c48e7044bc
Dijoyce <celton.jordan@outlook.fr>
2024-07-19 16:04:12 +0200

    add month on slider image && wip in components Events

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -4,42 +4,47 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const EventCard = ({
-  imageSrc,
-  imageAlt,
-  date = new Date(),
-  title,
-  label,
+  imageSrc = "/path/to/default/image.png", // Valeur par défaut
+  imageAlt = "image", // Valeur par défaut
+  date = new Date(), // Valeur par défaut
+  title = "Titre par défaut", // Valeur par défaut
+  label = "", // Valeur par défaut
   small = false,
   ...props
 }) => (
-    <div
-      data-testid="card-testid"
-      className={`EventCard${small ? " EventCard--small" : ""}`}
-      {...props}
-    >
-      <div className="EventCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-        <div className="EventCard__label">{label}</div>
-      </div>
-      <div className="EventCard__descriptionContainer">
-        <div className="EventCard__title">{title}</div>
-        <div className="EventCard__month">{getMonth(date)}</div>
-      </div>
+  <div
+    data-testid="card-testid"
+    className={`EventCard${small ? " EventCard--small" : ""}`}
+    {...props}
+  >
+    <div className="EventCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+      {label && <div className="EventCard__label">{label}</div>}{" "}
+      {/* Affiche le label uniquement s'il est défini */}
     </div>
-  );
+    <div className="EventCard__descriptionContainer">
+      <div className="EventCard__title">{title}</div>
+      <div className="EventCard__month">{getMonth(date)}</div>
+    </div>
+  </div>
+);
 
 EventCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
   imageAlt: PropTypes.string,
-  date: PropTypes.instanceOf(Date).isRequired,
-  title: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date),
+  title: PropTypes.string,
   small: PropTypes.bool,
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
 
 EventCard.defaultProps = {
+  imageSrc: "/path/to/default/image.png",
   imageAlt: "image",
+  date: new Date(),
+  title: "Titre par défaut",
   small: false,
-}
+  label: "",
+};
 
 export default EventCard;
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,51 +7,57 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+
+  // Tri des événements par date décroissante
+  const byDateDesc = data?.focus?.sort(
+    (evtA, evtB) => new Date(evtA.date) - new Date(evtB.date)
   );
+
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < (byDateDesc?.length ?? 0) - 1 ? index + 1 : 0),
       5000
     );
   };
+
   useEffect(() => {
     nextCard();
-  });
+  }, [index, byDateDesc?.length]);
+
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+      {byDateDesc?.map((event) => (
+        <div
+          key={event.id || event.date} // Utilisez une combinaison d'ID et de date si l'ID est absent
+          className={`SlideCard SlideCard--${
+            index === byDateDesc.findIndex((e) => e.date === event.date)
+              ? "display"
+              : "hide"
+          }`}
+        >
+          <img src={event.cover} alt={event.title} />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event, idx) => (
+            <input
+              key={event.id || idx} // Utiliser une combinaison d'ID et d'index si l'ID est absent
+              type="radio"
+              name="radio-button"
+              checked={index === idx}
+              readOnly
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
